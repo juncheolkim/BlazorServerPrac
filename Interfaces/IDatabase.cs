013ac582c9681155ce44a3b02b0c3e04c4ed4b8d
juncheolkim <kimjoon889@gmail.com>
2024-03-21 15:15:07 +0900

    DB Interface 작성

---
+++ Interfaces/IDatabase.cs
@@ -0,0 +1,18 @@
+﻿using BlazorServerProj.Models;
+
+namespace BlazorServerProj.Interfaces
+{
+    public interface IDatabase<T>
+    {
+        // 테이블 전체 데이터 조회
+        List<T>? Get();
+        // 테이블 특정 ID 조회
+        T GetDetail(int id);
+        // 새로운 행을 생성
+        void Create(T entity);
+        // 기존 행을 수정
+        void Update(int? id,T entity);
+        // 기존 행을 삭제
+        void Delete(int? id);
+    }
+}
